4264290b87f5b56f2a6778d98368b424d215539f
jukrap <jukrap628@gmail.com>
2024-05-01 12:31:55 +0900

    build: 패키지 추가 및 안드로이드 식별자 수정
    추가된 라이브러리 목록 -
    
    "date-fns"
    "dayjs"
    "react-native-fast-image"
    "react-native-gifted-chat"
    "react-native-image-crop-picker"
    "react-native-linear-gradient"
    "react-native-reanimated"
    "react-native-permissions"
    "react-native-select-dropdown"

--- android/app/src/main/java/com/gangsterdev/hobbyshare/MainActivity.kt
+++ android/app/src/main/java/com/gangsterdev/hobbyshare/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.gangsterdev.app
+package com.gangsterdev.hobbyshare
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/gangsterdev/hobbyshare/MainApplication.kt
+++ android/app/src/main/java/com/gangsterdev/hobbyshare/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.gangsterdev.app
+package com.gangsterdev.hobbyshare
 
 import android.app.Application
 import com.facebook.react.PackageList
